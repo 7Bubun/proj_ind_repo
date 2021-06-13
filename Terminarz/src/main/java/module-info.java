f50cd1afb4798b9f7deaf1b1b3c5e65c905b6e24
Jakub <j.grenda39@gmail.com>
2021-06-13 22:24:15 +0200

    Make ready for 'deployment'

--- Terminarz/src/main/java/module-info.java
+++ Terminarz/src/main/java/module-info.java
@@ -1,7 +1,5 @@
 module groupid.terminarz {
     requires javafx.controls;
-    requires javafx.graphics;   //not sure
-    requires javafx.base;       //not sure
     requires java.sql;
     exports groupid.terminarz;
     exports groupid.terminarz.logic;
